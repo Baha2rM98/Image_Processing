ccf51b23fa1c3f5ca1352da1af5c5fbdd618abc0
Bahador <49520007+Baha2rM98@users.noreply.github.com>
2019-07-23 20:29:25 +0430

    class face detection completed

--- src/imageProcessing/faceDetection/FaceDetection.java
+++ src/imageProcessing/faceDetection/FaceDetection.java
@@ -1,8 +1,36 @@
 package imageProcessing.faceDetection;
-import org.opencv.*;
+
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
 public class FaceDetection {
-    public FaceDetection() {
 
+    public FaceDetection() {
+    }
+
+    public void faceDetector(String imageInputPath, String imageOutPath) {
+        try {
+            loadLibrary();
+            CascadeClassifier detector = new CascadeClassifier();
+            System.out.println("face detecting started...");
+            final String fullFace = "resources/haarcascades/haarcascade_frontalface_alt.xml";
+            detector.load(fullFace);
+            Mat image = Imgcodecs.imread(imageInputPath);
+            MatOfRect faceDetector = new MatOfRect();
+            detector.detectMultiScale(image, faceDetector);
+            for (Rect rect : faceDetector.toArray()) {
+                Imgproc.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 0, 255), 5);
+            }
+            Imgcodecs.imwrite(imageOutPath, image);
+            System.out.println("face detecting ended...");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadLibrary() {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
 }
